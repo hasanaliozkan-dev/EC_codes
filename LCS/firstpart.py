739d63dfd8f05742d18614c224660d09a5239c05
hasanaliozkan-dev <hasanaliozkan@posta.mu.edu.tr>
2023-05-27 00:09:02 +0300

    "first section is almost done"

--- LCS/firstpart.py
+++ LCS/firstpart.py
@@ -1,6 +1,9 @@
+"""
+2-4 Multiplexer Problem
+"""
 import random
 
-#create random data that is the six bit string lol
+#create random data that is the six bit string list of lists
 random_data = [[random.randint(0,1) for i in range(6)] for j in range(100)]
 #lets flatten the inner arrays
 training_data_X = [''.join([str(i) for i in j]) for j in random_data]
@@ -9,6 +12,3 @@ training_data_y = [str(int(training_data_X[i][0])*2 + int(training_data_X[i][1])
 
 for x,y in zip(training_data_X,training_data_y):
     print(f"X Value -> {x} : y Value -> {y}")
-
-
-
